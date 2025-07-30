a9f1a6bd0624230dde0245f051404ce69c0dbcc5
alireza-astane <apoorhass@gmail.com>
2025-07-30 03:46:09 +0330

    automate build and run with bash scripting

--- docker/run.sh
+++ docker/run.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 docker compose down
 docker compose up -d
-
-
-
-# docker compose run --rm airflow bash -c "airflow db migrate"
-# docker compose run --rm airflow bash -c "airflow users create --username admin --firstname Admin --lastname User --role Admin --email admin@example.com --password admin"
-
-# sudo chown -R 500:500 ../airflow/logs
-# sudo chmod -R 755 ../airflow/logs
-
-# docker compose up -d airflow
